089b3041674069c88d1c867296fbf0b7a872c093
AdityaANH <aditya.n.h02@gmail.com>
2022-03-19 16:12:23 +0700

    menambahkan class SimpleDataBase

--- modul4_1302204078/Program.cs
+++ modul4_1302204078/Program.cs
@@ -1,1 +1,58 @@
-﻿namespace modul4_1302204078
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul4_1302204078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 02, 20));
+            SimpleDataBase<int> db = new SimpleDataBase<int>();
+            db.AddNewData(13);
+            db.AddNewData(02);
+            db.AddNewData(20);
+            db.PrintAllData();
+        }
+
+        class Penjumlahan
+        {
+            public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+            {
+                dynamic a = input1;
+                dynamic b = input2;
+                dynamic c = input3;
+
+                return a + b + c;
+            }
+
+        }
+
+        class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase()
+            {
+                this.storedData = new List<T>();
+                this.inputDates = new List<DateTime>();
+            }
+
+            public void AddNewData(T newData)
+            {
+                this.inputDates.Add(DateTime.Now);
+                this.storedData.Add(newData);
+            }
+
+            public void PrintAllData()
+            {
+                for (int i = 0; i < this.inputDates.Count; i++)
+                {
+                    Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                        ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+                }
+            }
+        }
+    }
+}
